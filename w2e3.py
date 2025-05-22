ca7f06ce4801a5c6dd72242c53a48efeb9ea943e
rxsemxrriee <winzaza2561@gmail.com>
2025-05-22 14:32:24 +0700

    update

--- w2e3.py
+++ w2e3.py
@@ -7,5 +7,5 @@ array1 = np.array([[1,2,3,3,2],
                    [13,14,15,5,6],
                    [16,17,28,3,4]], dtype=int)
 
-print(f'array_split:{np.array_split(array1,3 )}')
+print(f'array_split:{np.array_split(array1, 3)}')
 print(f'vsplit:{np.vsplit(array1, 2)}')
