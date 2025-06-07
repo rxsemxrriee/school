271e3c8c6c1774272698b4feba7a7d0deb766774
rxsemxrriee <winzaza2561@gmail.com>
2025-06-07 13:31:56 +0700

    week5 question5

---
+++ w5e5.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+day = []
+for i in range(1,11):
+    day.append(f"Day {i}")
+daytemp = [25,23,22,26,24,27,29,27,27,26]
+nighttemp = [18,17,16,19,18,20,21,18,17,20]
+
+plt.barh(day, nighttemp, color='blue')
+plt.barh(day,daytemp,left=nighttemp,color='black')
+plt.title("10 days temperature during the day and night")
+plt.xlabel('Temperature (in Celcius)')
+plt.ylabel('Days')
+plt.show()
